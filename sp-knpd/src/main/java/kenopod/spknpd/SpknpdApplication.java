500b3fc4ccc329d7cb8f3e9a8ef75d93371293e4
KlausBacker <boulanger.ncls@gmail.com>
2020-04-27 18:29:42 +0200

    Initial project version

---
+++ sp-knpd/src/main/java/kenopod/spknpd/SpknpdApplication.java
@@ -0,0 +1,13 @@
+package kenopod.spknpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpknpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpknpdApplication.class, args);
+    }
+
+}
---
+++ sp-knpd/src/test/java/kenopod/spknpd/SpknpdApplicationTests.java
@@ -0,0 +1,13 @@
+package kenopod.spknpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpknpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
